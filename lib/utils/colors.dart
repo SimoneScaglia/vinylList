d8a7cd4c962bc5496adf4be325d0358c45d94e8c
Simone Scaglia <simone.scaglia@dodifferent.it>
2024-09-13 17:57:18 +0200

    [DEV] Colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Colors{
+  static Color primaryColor = const Color(0xFF6B7280);
+  static Color secondaryColor = const Color(0xFFE5E7EB);
+  static Color accentColor = const Color(0xFFF97316);
+  static Color backgroundColor = const Color(0xFF374151);
+  static Color textPrimaryColor = const Color(0xFF1F2937);
+  static Color textSecondaryColor = const Color(0xFF9CA3AF);
+  static Color supportColor = const Color(0xFF10B981);
+}

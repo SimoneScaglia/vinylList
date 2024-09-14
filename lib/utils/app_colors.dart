c140d95d8e564a41ad122b3e1cd6a64bc17e3718
SimoneScaglia <simone311001@gmail.com>
2024-09-14 10:31:43 +0200

    [FIX] Rename Colors Page

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Colors{
+class AppColors{
   static Color primaryColor = const Color(0xFF6B7280);
   static Color secondaryColor = const Color(0xFFE5E7EB);
   static Color accentColor = const Color(0xFFF97316);
